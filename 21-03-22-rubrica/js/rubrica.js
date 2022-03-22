45a01d41b4a02f11a41c7bd7dcd1cf8a436f0b05
Chiara Zuccaro <chiara-zuc@libero.it>
2022-03-22 22:37:01 +0100

    perfezionamento rubrica

--- 21-03-22-rubrica/js/rubrica.js
+++ 21-03-22-rubrica/js/rubrica.js
@@ -1,35 +1,35 @@
 //                    ESERCITAZIONE 21-03-22
-//       
+     
 // const rubric = [
 //         {
 //             name: "Robert",
 //             surname: "Pattinson",
 //             phone: "349-539 2541 12",
-//             saves: true
+//             saves: "./star-with-five-points.png"
 //         },
 //         {
 //             name: "Homer",
 //             surname: "Simpson",
 //             phone: "375-520 5874 47",
-//             saves: true
+//             saves: "./star-with-five-points.png"
 //         },
 //         {
 //             name: "Patrick",
 //             surname: "Star",
 //             phone: "385-267 4286 51",
-//             saves: false
+//             saves: ""
 //         },
 //         {
 //             name: "Rick",
 //             surname: "Sanchez",
 //             phone: "341-456 7256 52",
-//             saves: true
+//             saves: "./star-with-five-points.png"
 //         }
 //         ,{
 //             name: "Bojack",
 //             surname: "Horseman",
 //             phone: "306-465 8222 64",
-//             saves: false
+//             saves: ""
 //         }
 //     ]
 const q = (selector) => document.querySelector(selector);
@@ -43,12 +43,28 @@ const phone = q("#phone");
 
 const visualList = () => {
     const listHTML = arrayObj.map((element) =>
-        `<li class="cards"><span>${element.name} ${element.surname}</span> <br> ${element.phone} ${element.saves}</li>`
+        `<li class="cards"><span>${element.name} ${element.surname}</span> <br> ${element.phone} <img src="${element.saves}"></li>`
     );
     return ul.innerHTML = listHTML.join("");
 };
 
+function fieldResetSaves() {
+    let ele = document.getElementsByName("saves");
+
+    for(let i=0; i < ele.length; i++) {        
+        ele[i].checked = false;
+    }
+}
+function fieldResetRemove() {
+    let ele = document.getElementsByName("rem");
+
+    for(let i=0; i < ele.length; i++) {        
+        ele[i].checked = false;
+    }
+}
+
 q("#add-btn").addEventListener("click", () => {
+    setTimeout(() => fieldResetSaves(), 100);
     if (name.value !== "" && surname.value !== "" && phone.value !== "") {
         let ans;
         for (const radioSave of radioSaves) {
@@ -57,12 +73,22 @@ q("#add-btn").addEventListener("click", () => {
                 break;
             }
         }
-        arrayObj.push({
-            name: `${name.value}`,
-            surname: `${surname.value}`,
-            phone: `${phone.value}`,
-            saves: `${ans}`
-        });
+        if(ans === "true") {            
+            arrayObj.push({
+                name: `${name.value}`,
+                surname: `${surname.value}`,
+                phone: `${phone.value}`,
+                saves: `./star-with-five-points.png`
+            });
+        } else {
+            console.log(ans);
+            arrayObj.push({
+                name: `${name.value}`,
+                surname: `${surname.value}`,
+                phone: `${phone.value}`,
+                saves: ``
+            });
+        }
         console.log("aggiunto");
         localStorage.setItem("rubric", JSON.stringify(arrayObj));
         console.log("anche in storage");
