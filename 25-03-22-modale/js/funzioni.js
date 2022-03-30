fdd1df2a3ae29876998e9b9dfd2bf7c8726a7663
Chiara Zuccaro <chiara-zuc@libero.it>
2022-03-30 21:47:04 +0200

    section added by javascript

--- 25-03-22-modale/js/funzioni.js
+++ 25-03-22-modale/js/funzioni.js
@@ -1,6 +1,7 @@
 //                              FUNZIONI ESERCITAZIONE 25-03-22
 
 const q = (selector) => document.querySelector(selector);
+let genresList = [];
 
 const formatMinText = (text) => text.split(" ").slice(0, 15).join(" ") + " ...";
 
@@ -108,7 +109,7 @@ const deleteCard = (card, index) => {
         },
     });
 
-    card.innerHTML = `<h2 class="erased">Movie erased</h2>`;
+    card.innerHTML = `<h2 class="erased">Movie deleted</h2>`;
     setTimeout(() => location.reload(), 2000);
 }
 const zoomMov = (cardObj) => {
@@ -153,7 +154,6 @@ const zoomMov = (cardObj) => {
         q(".zoom-card").removeChild(q(".cards"));
     });
 }
-
 const choiceCard = (card, list) => {
     const choiceSel = document.querySelectorAll(".new-choice");   
     const cardEl = [];
@@ -247,7 +247,9 @@ const createGenre = (string) => {
 
     divNewGenre.append(labelNew, inputNew);
 
-    q(".check-flex").appendChild(divNewGenre);
+    setTimeout(() => {
+        q(".check-flex").appendChild(divNewGenre);
+    }, 50)
 }
 
 const loadGenre = (list) => {
@@ -273,4 +275,176 @@ const addGenre = (userValue, list) => {
 }
 
 
-export { q, zoomMov, createCard, getNew, getApi, deleteCard, choiceCard, loadGenre, addGenre }
+
+const formTitle = () => {    
+    const divTitle = document.createElement("div");
+    
+    const labelTitle = document.createElement("label");
+    const inputTitle = document.createElement("input");
+
+    divTitle.classList.add("user");
+    
+    labelTitle.setAttribute("for", "title");
+    inputTitle.setAttribute("id", "title");
+    inputTitle.setAttribute("type", "text");
+    inputTitle.setAttribute("placeholder", "here your title...");
+
+    labelTitle.textContent = "Title: "
+
+    divTitle.append(labelTitle, inputTitle);
+
+    return divTitle;
+}
+const formPoster = () => {
+    const divPoster = document.createElement("div");
+
+    const labelPoster = document.createElement("label");
+    const inputPoster = document.createElement("input");
+    
+    divPoster.classList.add("user");
+    
+    labelPoster.setAttribute("for", "poster");
+    inputPoster.setAttribute("id", "poster");
+    inputPoster.setAttribute("type", "text");
+    inputPoster.setAttribute("placeholder", "an url is needed...");
+
+    labelPoster.textContent = "Poster: "
+
+    divPoster.append(labelPoster, inputPoster);
+
+    return divPoster;
+}
+const formDesc = () => {
+    const divDesc = document.createElement("div");
+
+    const labelDesc = document.createElement("label");
+    const inputDesc = document.createElement("input");
+    
+    divDesc.classList.add("user");
+    
+    labelDesc.setAttribute("for", "description");
+    inputDesc.setAttribute("id", "description");
+    inputDesc.setAttribute("type", "text");
+    inputDesc.setAttribute("placeholder", "what is it talking about");
+
+    labelDesc.textContent = "A little description: "
+
+    divDesc.append(labelDesc, inputDesc);
+    return divDesc;
+}
+const formYear = () => {
+    const divYear = document.createElement("div");    
+
+    const labelYear = document.createElement("label");
+    const inputYear = document.createElement("input");
+
+    divYear.classList.add("user");
+    
+    labelYear.setAttribute("for", "year");
+    inputYear.setAttribute("id", "year");
+    inputYear.setAttribute("type", "text");
+    inputYear.setAttribute("placeholder", "when did it was produced");
+
+    labelYear.textContent = "Year: "
+
+    divYear.append(labelYear, inputYear);
+    return divYear;
+}
+const formGen = () => {
+    const divGen = document.createElement("div");
+    const divGenList = document.createElement("div");
+    const divGenAdd = document.createElement("div");
+
+    const labelGen = document.createElement("label");
+    const inputGen = document.createElement("input");
+    const btnGen = document.createElement("button");
+    
+    const pGen = document.createElement("p");
+
+    pGen.textContent = "Genre: "
+
+    divGen.classList.add("genres");
+
+    divGenList.classList.add("check-flex");
+
+    divGenAdd.classList.add("add-genre");
+
+    labelGen.setAttribute("for", "other");
+    inputGen.setAttribute("id", "other");
+    inputGen.setAttribute("type", "text");
+    inputGen.setAttribute("placeholder", "put here a new genre ...");
+    btnGen.setAttribute("type", "submit");
+    btnGen.setAttribute("id", "plus");
+
+    btnGen.textContent = "+";
+    labelGen.textContent = "Other ";
+
+    divGenAdd.append(labelGen, inputGen, btnGen);
+
+    divGen.append( pGen, divGenList, divGenAdd);
+
+    return divGen;
+}
+
+const addForm = () => {
+    const formEl = document.createElement("form");
+    const btnAdd = document.createElement("button");
+
+    btnAdd.setAttribute("id", "sub");
+    btnAdd.setAttribute("type", "submit");
+    btnAdd.textContent = "Add this movie";
+
+    //  FORM
+    formEl.append( formTitle(), formPoster(), formYear(), formDesc(), formGen(), btnAdd);
+
+    try {
+        genresList = localStorage.getItem("genres").split(",");
+    } catch {
+        genresList = ["Horror", "Action", "TVSeries"];
+    }
+
+    loadGenre(genresList);
+
+    q(".user-add").appendChild(formEl);
+
+
+    q("#sub").addEventListener("click", (event) => {
+        event.preventDefault();
+    
+        fetch("https://edgemony-backend.herokuapp.com/movies", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(getNew()),
+        }).then(() => location.reload());
+    
+    
+        q("#title").value = "";
+        q("#description").value = "";
+        q("#year").value = "";
+        q("#poster").value = "";
+
+        q(".user-add").innerHTML = ``;
+    });
+
+    q("#plus").addEventListener("click", (event) => {
+        event.preventDefault();    
+        let genResults = [];
+        const genrePlus = q("#other");
+    
+        if(genrePlus.value == "")
+            alert("You have to insert a value!");
+        else {    
+            console.log(genrePlus.value);    
+            genResults = addGenre(genrePlus.value, genresList);
+        }
+            
+        localStorage.setItem("genres", genResults);
+        genrePlus.value == "";
+        // location.reload();
+    });
+
+}
+
+export { q, zoomMov, createCard, getNew, getApi, deleteCard, choiceCard, loadGenre, addGenre, addForm }
--- 25-03-22-modale/js/modale.js
+++ 25-03-22-modale/js/modale.js
@@ -1,14 +1,4 @@
-import { q, createCard, getNew, getApi, choiceCard, loadGenre, addGenre } from "./funzioni.js";
-
-let genresList = [];
-
-try {
-    genresList = localStorage.getItem("genres").split(",");
-} catch {
-    genresList.push("Horror", "TVSeries", "Fantasy", "Animation");
-}
-
-loadGenre(genresList);
+import { q, createCard, getApi, choiceCard, addForm } from "./funzioni.js";
 
 const movieList = [];
 
@@ -33,37 +23,7 @@ getApi()
         });
 });
 
-q("#sub").addEventListener("click", (event) => {
-    event.preventDefault();
 
-    fetch("https://edgemony-backend.herokuapp.com/movies", {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json",
-        },
-        body: JSON.stringify(getNew()),
-    }).then(() => location.reload());
-
-
-    q("#title").value = "";
-    q("#description").value = "";
-    q("#year").value = "";
-    q("#poster").value = "";
-});
-
-
-q("#plus").addEventListener("click", (event) => {
-    event.preventDefault();    
-    let genResults = [];
-    const genrePlus = q("#other");
-
-    if(genrePlus.value == "")
-        alert("Devi inserire un valore per aggiungere un genere!");
-    else {    
-        console.log(genrePlus.value);    
-        genResults = addGenre(genrePlus.value, genresList);
-    }
-        
-    localStorage.setItem("genres", genResults);
-    location.reload();
+q("#add-nav").addEventListener("click", () => {
+    addForm();
 });
